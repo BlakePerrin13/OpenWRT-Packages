a40825f8dff8ebb95817a5bab7f33422402f6b74
Blake <blake.perrin13@gmail.com>
2023-12-01 19:55:43 -0600

    idk

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='python_hello',
     version='0.1.0',
-    url='https://github.com/BlakePerrin13/OpenWRT-Packages/tree/main/Packages/Python.git',
+    url='https://github.com/BlakePerrin13/OpenWRT-Packages.git',
     author='Blake Perrin',
     author_email='blake.perrin13@gmail.com',
     description='Python Hello World',
     packages=find_packages(),    
     install_requires=[''],
     py_modules=[
-        'hello.py',
+        'python_hello.py',
     ],
 )
